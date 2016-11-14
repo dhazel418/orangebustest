fcf595005cf9fafdd8f845f967e34ce5f58b0923
dhazel418 <david.hazel@enchaine.com>
2016-11-14 11:27:21 +0000

    Implementation of IPackOfCards interface, with methods yet to be implemented via TDD.

---
+++ CodingTest/CodingTest/SkeletonCode/CardGame/PackOfCards.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace SkeletonCode.CardGame
+{
+  public class PackOfCards : IPackOfCards
+  {
+    private IReadOnlyCollection<ICard> _cardCollection; 
+
+    public PackOfCards()
+    {
+      IList<ICard> cardList = new List<ICard>();
+      for (EnumSuit suit = EnumSuit.Clubs; suit <= EnumSuit.Diamonds; suit++)
+      {
+        for (EnumCardNumber cardNo = EnumCardNumber.Two; cardNo <= EnumCardNumber.Ace; cardNo++)
+        {
+          cardList.Add(new Card(suit, cardNo));
+        }
+      }
+
+      _cardCollection = new ReadOnlyCollection<ICard>(cardList);
+    }
+
+    #region Implementation of IEnumerable
+
+    /// <summary>
+    /// Returns an enumerator that iterates through the collection.
+    /// </summary>
+    /// <returns>
+    /// A <see cref="T:System.Collections.Generic.IEnumerator`1"/> that can be used to iterate through the collection.
+    /// </returns>
+    public IEnumerator<ICard> GetEnumerator()
+    {
+      return _cardCollection.GetEnumerator();
+    }
+
+    /// <summary>
+    /// Returns an enumerator that iterates through a collection.
+    /// </summary>
+    /// <returns>
+    /// An <see cref="T:System.Collections.IEnumerator"/> object that can be used to iterate through the collection.
+    /// </returns>
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+      return GetEnumerator();
+    }
+
+    #endregion
+
+    #region Implementation of IReadOnlyCollection<out ICard>
+
+    /// <summary>
+    /// Gets the number of elements in the collection.
+    /// </summary>
+    /// <returns>
+    /// The number of elements in the collection. 
+    /// </returns>
+    public int Count { get { return _cardCollection.Count; } }
+
+    #endregion
+
+    #region Implementation of IPackOfCards
+
+    public void Shuffle()
+    {
+      throw new NotImplementedException();
+    }
+
+    public ICard TakeCardFromTopOfPack()
+    {
+      throw new NotImplementedException();
+    }
+
+    #endregion
+  }
+}
